4989e2e9b89f0da1814be239abef4f2da12ffcef
Yogesh Rajput <yogeshrajput335@gmail.com>
2018-08-20 20:09:21 +0530

    gps

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { PageComponent } from './components/page/page.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { GpsComponent } from './components/gps/gps.component';
+import { MutualfundComponent } from './components/mutualfund/mutualfund.component';
 
 export const ROUTES: Routes = [
     // routes from pages
     {path: 'home', component: HomeComponent, data: {title: 'Home'}},
     {path: 'page', component: PageComponent, data: {title: 'Page'}},
     {path: 'gps', component: GpsComponent, data: {title: 'gps'}},
+    {path: 'mutualfund', component: MutualfundComponent, data: {title: 'Mutual Fund'}},
     {path: '**', component: HomeComponent},
     { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
